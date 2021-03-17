eaae118d48aac3d5c34aa33353615fc6c86ab2e5
simeonis <56325172+simeonis@users.noreply.github.com>
2021-03-17 19:16:13 -0400

    Added OAuth token validation

--- screens/Screens.js
+++ screens/Screens.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Button, SafeAreaView, Text } from 'react-native';
 import { general } from '../assets/styles';
 import * as AuthSession from 'expo-auth-session';
+
+// Envi
 import { TWITCH_CLIENT_ID, TWITCH_REDIRECT_URI } from "@env";
 
 export const Splash = () => {
@@ -16,15 +18,19 @@ export const Home = ({route, navigation}) => {
 
     // State variables
     const [token, setToken] = React.useState("NULL");
+    const [username, setUsername] = React.useState("NULL");
+    console.log(token);
 
     // OnMount
     React.useEffect(() => {
         setToken(route.params.token ? route.params.token : "NULL");
+        setUsername(route.params.username ? route.params.username : "NULL");
     }, []);
 
     return (
         <SafeAreaView style={general.container}>
             <Text style={general.title}>Home Screen</Text>
+            <Text>Username: {username}</Text>
             <Text>Token: {token}</Text>
         </SafeAreaView>
     );
@@ -34,10 +40,15 @@ export const Twitch = ({route, navigation}) => {
 
     // Twitch API
     const TWITCH_SCOPES = ["chat:edit", "chat:read"];
-    const TWITCH_URL = "https://id.twitch.tv/oauth2/authorize";
+    const TWITCH_BASE_URL = "https://id.twitch.tv/oauth2/";
+    const TWITCH_AUTH_PATH = "authorize";
+    const TWITCH_VALID_PATH = "validate";
 
-    // AuthRequest
-    const [request, response, promptAsync] = AuthSession.useAuthRequest(
+    // Temporary (Missing DataBase)
+    let TEMP_TOKEN = "2m9m5crfbuoptntx1jchdjrr65jocr";
+
+    // Generate new token
+    const [authRequest, authResponse, authPrompt] = AuthSession.useAuthRequest(
         {
             responseType: AuthSession.ResponseType.Token,
             clientId: TWITCH_CLIENT_ID,
@@ -45,25 +56,49 @@ export const Twitch = ({route, navigation}) => {
             redirectUri: TWITCH_REDIRECT_URI,
         },
         {
-          authorizationEndpoint: TWITCH_URL
+          authorizationEndpoint: TWITCH_BASE_URL + TWITCH_AUTH_PATH
         }
     );
 
-    // OnMount
+    // Validate Token
+    const validateToken = async (access_token) => {
+        fetch(TWITCH_BASE_URL + TWITCH_VALID_PATH, {
+            method: 'GET',
+            headers: {
+                "Authorization": `OAuth ${access_token}`
+            }
+        })
+        .then((validResponse) => validResponse.json())
+        .then((json) => {
+            // Valid non-expired token
+            if (json.expires_in && json.expires_in > 0) {
+                navigation.navigate("Home", { token: access_token, username: json.login})
+            } 
+            // Invalid token, ask for another
+            else {
+                authPrompt({useProxy: true, TWITCH_REDIRECT_URI});
+            }
+        })
+        .catch((error) => {
+            console.error(`Unsucessful validation request: ${error}`);
+        });
+    }
+
+    // OnTokenGeneration
     React.useEffect(() => {
         // AuthRequest result
-        if (response?.type === 'success') {
-          const { access_token } = response.params;
-          navigation.navigate("Home", { token: access_token })
+        if (authResponse?.type === 'success') {
+            const { access_token } = authResponse.params;
+            TEMP_TOKEN = access_token;
         }
-    }, [response]);
+    }, [authResponse]);
 
     return (
         <SafeAreaView style={general.container}>
             <Text style={general.title}>Twitch Screen</Text>
             <Button 
-            title="Authorize" 
-            onPress={() => promptAsync({useProxy: true, TWITCH_REDIRECT_URI})}/>
+            title="Validate Token" 
+            onPress={() => validateToken(TEMP_TOKEN)}/>
         </SafeAreaView>
     );
 }
